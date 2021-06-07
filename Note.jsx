bbd8150ebdbf59557eefe951653be40e86944170
DarshanKatale <83976306+DarshanKatale@users.noreply.github.com>
2021-06-07 13:06:14 +0530

    Add files via upload

---
+++ Note.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+
+function Note() {
+  return (
+    <div className="note"
+    <h1>'Wh' words list</h1>
+    <p>
+    •What
+    •Where
+    •Who
+    •When
+    •Why
+    </p>
+    </div>
+    );
+}
+
+export default Note;
